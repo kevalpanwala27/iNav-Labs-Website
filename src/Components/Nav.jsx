905a055de7b5eda34a4a60b958a49e124914fd96
Keval Panwala <kevalpanwala@Kevals-Air.mshome.net>
2024-01-07 13:57:32 +0530

    Full website completed

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import logo from "../assets/logo1.png";
-import { FiAlignJustify } from "react-icons/fi";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Nav = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -14,13 +14,14 @@ const Nav = () => {
     rest: { scale: 1, borderColor: "transparent" },
     hover: { scale: 1.1, borderColor: "" },
   };
+
   return (
     <header className="flex justify-between p-6 m-5">
       <div className="flex items-center gap-2">
         <img
           src={logo}
           alt="company logo"
-          className="md:w-10 md:h-10 w-6 h-6 rounded-2xl"
+          className="md:w-10 md:h-10 w-6 h-6 md:rounded-2xl rounded-lg"
         />
         <div>
           <span className="font-semibold md:text-3xl text-xl text-green-500 tracking-wider font-LexendDeca">
@@ -31,7 +32,12 @@ const Nav = () => {
           NAV LABS
         </span>
       </div>
-      <div className="md:flex hidden md:visible gap-10 border border-gray-400 px-10 rounded-full shadow-md shadow-gray-200 items-center bg-stone-100">
+
+      <div
+        className={`md:flex hidden md:visible gap-10 border border-gray-400 px-10 rounded-full shadow-md shadow-gray-200 items-center bg-stone-100 ${
+          isNavOpen ? "flex flex-col" : "hidden"
+        }`}
+      >
         <motion.div
           variants={navItemVariants}
           whileHover="hover"
@@ -48,7 +54,7 @@ const Nav = () => {
           initial="rest"
           className="hover:bg-gray-200 border hover:border-gray-500 rounded-full cursor-pointer transition-all duration-300 px-4 font-LexendDeca tracking-widest"
         >
-          <a href="#service" className="text-sm">
+          <a href="#service" className="text-md">
             SERVICES
           </a>
         </motion.div>
@@ -58,7 +64,7 @@ const Nav = () => {
           initial="rest"
           className="hover:bg-gray-200 border hover:border-gray-500 rounded-full cursor-pointer transition-all duration-300 px-4 font-LexendDeca tracking-widest"
         >
-          <a href="#Whatwedo" className="text-sm">
+          <a href="#Whatwedo" className="text-md">
             ABOUT US
           </a>
         </motion.div>
@@ -68,7 +74,7 @@ const Nav = () => {
           initial="rest"
           className="hover:bg-gray-200 border hover:border-gray-500 rounded-full cursor-pointer transition-all duration-300 px-4 font-LexendDeca tracking-widest"
         >
-          <a className="text-sm" href="#team">
+          <a className="text-md" href="#team">
             OUR TEAM
           </a>
         </motion.div>
@@ -78,16 +84,65 @@ const Nav = () => {
           initial="rest"
           className="hover:bg-gray-200 border hover:border-gray-500 rounded-full cursor-pointer transition-all duration-300 px-4 font-LexendDeca tracking-widest"
         >
-          <a className="text-sm" href="#contactus">
+          <a className="text-md" href="#contactus">
             CONTACT US
           </a>
         </motion.div>
       </div>
-      <div className="md:hidden">
-        <button onClick={toggleNav} className="cursor-pointer px-4">
-          <FiAlignJustify />
-        </button>
+
+      {/* Mobile Toggle Button */}
+      <div
+        className="md:hidden"
+        style={{ position: "absolute", top: "50px", right: "50px" }}
+      >
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          whileHover={{ scale: 1.1 }}
+          onClick={toggleNav}
+          className={`cursor-pointer ${isNavOpen ? "hidden" : ""}`}
+        >
+          <FaBars className="text-lg" />
+        </motion.div>
+        {isNavOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ type: "spring", stiffness: 200, damping: 25 }}
+            className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 z-50"
+            onClick={toggleNav}
+          />
+        )}
+        {isNavOpen && (
+          <FaTimes onClick={toggleNav} className="cursor-pointer" />
+        )}
       </div>
+
+      {/* Responsive Navigation */}
+      <AnimatePresence>
+        {isNavOpen && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.8 }}
+            transition={{ type: "spring", stiffness: 200, damping: 25 }}
+            className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 z-50"
+            onClick={toggleNav}
+          >
+            <div className="gap-16 shadow-md text-center bg-white p-8 rounded">
+              <div className="flex flex-col space-y-2">
+                <FaTimes onClick={toggleNav} className="cursor-pointer" />
+                <a href="#home">Home</a>
+                <a href="#service">Services</a>
+                <a href="#Whatwedo">About Us</a>
+                <a href="#team">Our Team</a>
+                <a href="#contactus">Contact Us</a>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </header>
   );
 };
